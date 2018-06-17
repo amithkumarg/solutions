c3d582bc625470a54b57e22b5087208bc6da3030
Amith Kumar <amith.kumar@pegs.com>
2018-06-17 00:28:04 -0700

    Int Reader

---
+++ src/main/java/com/oss/shop/Assignment/InputIntRead.java
@@ -0,0 +1,29 @@
+package com.oss.shop.Assignment;
+
+import java.util.Scanner;
+
+public class InputIntRead {
+	
+	public static void main(String[] args){
+		System.out.println("UserNumber entered:" + userNumber());
+	}
+	
+	public static int userNumber(){
+
+		try (Scanner scanner = new Scanner(System.in)) {
+		    String msg = "Please, enter a positive integer:";
+		    int n = 0;
+
+		    while(n <= 0){	            
+	            System.out.println(msg);
+	            if(scanner.hasNextInt()){
+	            	n = scanner.nextInt();
+	            } else {
+	            	scanner.next();
+	            }
+	        }
+
+		    return n;
+		}
+    }
+}	
